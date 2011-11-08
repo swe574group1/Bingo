d3c63d6a11f6f4b6fe018cb2abbb1306cb5fe8a4
fcdemirci@gmail.com <fcdemirci@gmail.com@6cf4c153-00bf-4111-7615-4be471eb276b>
2011-11-08 18:15:17 +0000

    minimal connection between offer creation pages

--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -7,51 +7,53 @@ import controllers.Secure.Security;
 import models.City;
 import models.County;
 import models.User;
+import models.Offer;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
+import play.data.validation.*;
 
 public class Application extends Controller
 {   
-	@Before
-    static void setConnectedUser() {
-//    System.out.println("APPLICATION IS IN CONNECTED USER");
-    boolean isConnected = Security.isConnected();
-    renderArgs.put("isLoggedIn", isConnected);
+    @Before
+	static void setConnectedUser() {
+	//    System.out.println("APPLICATION IS IN CONNECTED USER");
+	boolean isConnected = Security.isConnected();
+	renderArgs.put("isLoggedIn", isConnected);
 	if(isConnected) {
-        User user = User.find("byEmail", Security.connected()).first();
-        renderArgs.put("user", user);
-    }
-}
-	public static void index()
-	{
-	    render();
-		// Create a new user and save it
-/*		User newUser = new User();
-		newUser.email = "bob@gmail.com";
-		newUser.password = "secret";
-        newUser.nickname = "bob";
-		newUser.fullname = "Bob";
-		newUser.address = "address";
-		//newUser.avatar = new UploadedFile();
-		newUser.balance = 0;
-		newUser.birthday = new Date();
-		newUser.city = new City();
-		newUser.county = new County();
-		newUser.isAdmin = false;
-		newUser.job = "job";
-		newUser.phone = "phone";
-		newUser.registrationDate = new Date();
-		newUser.reputation = 0;
-		newUser.status = User.Status.WAITING_CONFIRMATION;
-		newUser.save();
-
-		// Retrieve the user with e-mail address bob@gmail.com
-		User bob2 = User.find("byEmail", "bob@gmail.com").first();
-
-		render(bob2);*/
-		
+	    User user = User.find("byEmail", Security.connected()).first();
+	    renderArgs.put("user", user);
 	}
+    }
+    public static void index()
+    {
+	render();
+	// Create a new user and save it
+	/*		User newUser = new User();
+			newUser.email = "bob@gmail.com";
+			newUser.password = "secret";
+			newUser.nickname = "bob";
+			newUser.fullname = "Bob";
+			newUser.address = "address";
+			//newUser.avatar = new UploadedFile();
+			newUser.balance = 0;
+			newUser.birthday = new Date();
+			newUser.city = new City();
+			newUser.county = new County();
+			newUser.isAdmin = false;
+			newUser.job = "job";
+			newUser.phone = "phone";
+			newUser.registrationDate = new Date();
+			newUser.reputation = 0;
+			newUser.status = User.Status.WAITING_CONFIRMATION;
+			newUser.save();
+
+			// Retrieve the user with e-mail address bob@gmail.com
+			User bob2 = User.find("byEmail", "bob@gmail.com").first();
+
+			render(bob2);*/
+		
+    }
 
     public static void register() {
 	render();
@@ -61,15 +63,25 @@ public class Application extends Controller
 	render();
     }
 
-    public static void finalizeOffer() {
-	render();
+    public static void finalizeOffer(Offer offerItem) {
+	render(offerItem);
     }
 
     public static void createRequest() {
 	render();
     }
 
+    public static void doCreateOfferItem(@Valid Offer offerItem) {
+	if (validation.hasErrors()) {
+	    params.flash();
+	    validation.keep();
+	    createRequest();
+	}
+	finalizeOffer(offerItem);
+    }
+
     public static void finalizeRequest() {
 	render();
     }
+
 }
