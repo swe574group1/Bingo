450991d4abc3c48b52e64541d3638375f72c55e1
ertugkaramatli@gmail.com <ertugkaramatli@gmail.com@6cf4c153-00bf-4111-7615-4be471eb276b>
2011-12-04 18:42:17 +0000

    user stuff

--- src/app/controllers/Users.java
+++ src/app/controllers/Users.java
@@ -9,25 +9,30 @@ public class Users extends BaseController
 {
 
     public static void register() {
-	render(new User());
+        User user = new User();
+        renderTemplate("Users/form.html", user);
     }
 
-    public static void doCreateUserEntry(@Valid User user) {
-	if (validation.hasErrors()) {
-	    params.flash();
-	    validation.keep();
-	    register();
-	}
-	user.save();
-	displayUserInfo(user);
+    public static void save(@Valid User user) {
+        boolean isCreate = (user.id == null);
+
+    	if (validation.hasErrors()) {
+    	    params.flash();
+    	    validation.keep();
+    	    register();
+    	}
+    	user.save();
+
+    	if (isCreate) {
+    	    success(user.id);
+    	} else {
+    	    ownProfile();
+    	}
     }
 
-    public static void displayUserInfo(User user) {
-	render(user);
-    }
-
-    public static void finalizeUser() {
-	render();
+    public static void success(Long userId) {
+        User user = User.findById(userId);
+        render(user);
     }
 
     public static void profile(Long userId) {
@@ -43,12 +48,8 @@ public class Users extends BaseController
     }
 
     public static void editProfile() {
-	User connectedUser = getConnectedUser();
-	showEditableProfile(connectedUser.id);
-    }
-
-    public static void showEditableProfile(Long userId) {
-	User user = User.findById(userId);
-	render(user);
+        User connectedUser = getConnectedUser();
+        User user = User.findById(connectedUser.id);
+        renderTemplate("Users/form.html", user);
     }
 }
